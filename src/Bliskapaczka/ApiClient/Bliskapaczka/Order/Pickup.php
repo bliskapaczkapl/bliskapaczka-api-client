2a3d6c13d90b3b9050d252b31d31c68260881db1
Paweł Karbowniczek <pkarbowniczek@divante.pl>
2020-03-23 15:05:36 +0100

    feature/WiW-127 Add pickup class

---
+++ src/Bliskapaczka/ApiClient/Bliskapaczka/Order/Pickup.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Bliskapaczka\ApiClient\Bliskapaczka\Order;
+
+use Bliskapaczka\ApiClient\BliskapaczkaInterface;
+use Bliskapaczka\ApiClient\AbstractBliskapaczka;
+
+class Pickup extends AbstractBliskapaczka implements BliskapaczkaInterface
+{
+    const REQUEST_URL = 'orders/pickup';
+
+    /** @var string */
+    private $orderId = null;
+
+    public function setOrderId( $orderId )
+    {
+        $this->orderId = $orderId;
+    }
+
+    /**
+     * @param array $data
+     *
+     * @return bool
+     */
+    public function validate(array $data)
+    {
+        return true;
+    }
+
+    /**
+     * @param array $data
+     *
+     * @return json $response
+     */
+    public function create(array $data)
+    {
+        if (isset($this->orderId)) {
+            $data['orderNumbers'] = $this->orderId;
+        }
+        return $this->doCall($this->getUrl(), json_encode($data), array(), 'POST');
+    }
+}
