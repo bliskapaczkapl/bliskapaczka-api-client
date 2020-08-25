f9766e786434ab13390bec839eba95ec611a57c8
Marek Warczykowski <4921750+markoWar@users.noreply.github.com>
2020-08-25 12:12:09 +0200

    Code fixes

--- src/Bliskapaczka/ApiClient/Bliskapaczka/Order/AvailableServices.php
+++ src/Bliskapaczka/ApiClient/Bliskapaczka/Order/AvailableServices.php
@@ -12,35 +12,35 @@ use Bliskapaczka\ApiClient\BliskapaczkaInterface;
 class AvailableServices extends AbstractBliskapaczka implements BliskapaczkaInterface
 {
 
-	const REQUEST_URL = 'order/services';
+    const REQUEST_URL = 'order/services';
 
-	/**
-	 * Get avaible services
-	 *
-	 * @see https://api-docs.sandbox-bliskapaczka.pl/#_available_services
-	 *
-	 * @param array $data
-	 *        	Data to sent for the endpoint
-	 * @param array $headers
-	 *        	Extra headers to sent
-	 *        	
-	 * @return string Respone json body
-	 */
-	public function get(array $data, array $headers = [])
-	{
-		$this->validate($data);
-		return $this->doCall($this->getUrl(), \json_encode($data), $headers, 'POST');
-	}
+    /**
+     * Get avaible services
+     *
+     * @see https://api-docs.sandbox-bliskapaczka.pl/#_available_services
+     *
+     * @param array $data
+     *          Data to sent for the endpoint
+     * @param array $headers
+     *          Extra headers to sent
+     *
+     * @return string Respone json body
+     */
+    public function get(array $data, array $headers = [])
+    {
+        $this->validate($data);
+        return $this->doCall($this->getUrl(), \json_encode($data), $headers, 'POST');
+    }
 
-	/**
-	 *
-	 * {@inheritdoc}
-	 * @see \Bliskapaczka\ApiClient\BliskapaczkaInterface::validate()
-	 */
-	public function validate(array $data)
-	{
-		$validator = $this->getValidator();
-		$validator->setData($data);
-		$validator->validate();
-	}
+    /**
+     *
+     * {@inheritdoc}
+     * @see \Bliskapaczka\ApiClient\BliskapaczkaInterface::validate()
+     */
+    public function validate(array $data)
+    {
+        $validator = $this->getValidator();
+        $validator->setData($data);
+        $validator->validate();
+    }
 }
