0629a021e14ad0bc4e731401ff47ef75f208c5b6
Marek Warczykowski <4921750+markoWar@users.noreply.github.com>
2020-09-03 12:15:42 +0200

    Fix test.

--- tests/unit/Bliskapaczka/ApiClient/Validator/OrderTest.php
+++ tests/unit/Bliskapaczka/ApiClient/Validator/OrderTest.php
@@ -216,7 +216,7 @@ class OrderTest extends TestCase
      */
     public function testCodPayoutBankAccountNumberWithCountyCode()
     {
-        $this->orderData['codPayoutBankAccountNumber'] = 'PL16102019120000910201486273';
+        $this->orderData['codPayoutBankAccountNumber'] = 'PL16102019120000910201486278';
 
         $order = new Order();
         $order->setData($this->orderData);
